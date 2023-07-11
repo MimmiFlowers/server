382906ecb0466fc5e5dcc98995552894c0dff010
LYMGo <igors.puris@gmail.com>
2023-07-12 00:41:34 +0200

    Mocked data has been added. FastAPI has been implemented.

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import requests
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8500",
+    "http://localhost:3000",
+    "https://imgur.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/favorite')
 def favorite_info():
@@ -10,25 +26,25 @@ def favorite_info():
         {
             "name": "Favorite Bouqet 1",
             "description": "This description is for bouqet 1",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "11.11"
         },
         {
             "name": "Favorite Bouqet 2",
             "description": "This description is for bouqet 2",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "22.22"
         },
         {
             "name": "Favorite Bouqet 3",
             "description": "This description is for bouqet 3",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "33.33"
         },
         {
             "name": "Favorite Bouqet 4",
             "description": "This description is for bouqet 4",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "44.44"
         }
     ]
@@ -42,25 +58,25 @@ def season_info():
         {
             "name": "Season Bouqet 1",
             "description": "This description is for bouqet 1",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "11.11"
         },
         {
             "name": "Season Bouqet 2",
             "description": "This description is for bouqet 2",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "22.22"
         },
         {
             "name": "Season Bouqet 3",
             "description": "This description is for bouqet 3",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "33.33"
         },
         {
             "name": "Season Bouqet 4",
             "description": "This description is for bouqet 4",
-            "picture": "",
+            "picture": "https://i.imgur.com/0NCCt4A.png",
             "price": "44.44"
         }
     ]
