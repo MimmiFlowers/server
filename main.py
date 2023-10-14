f5da6e1d9126720ff7566d35e816e4faa2c219a3
LYMGo <igors.puris@gmail.com>
2023-10-15 01:50:07 +0200

    Mocked data has been extended with one new rout.

--- main.py
+++ main.py
@@ -22,62 +22,133 @@ app.add_middleware(
 @app.get('/favorite')
 def favorite_info():
     return {
-    "data": [
-        {
-            "name": "Favorite Bouqet 1",
-            "description": "This description is for bouqet 1",
-            "picture": "https://i.imgur.com/ScOiPDx.jpg",
-            "price": "1111"
-        },
-        {
-            "name": "Favorite Bouqet 2",
-            "description": "This description is for bouqet 2",
-            "picture": "https://i.imgur.com/RgtoxyG.jpg",
-            "price": "2222"
-        },
-        {
-            "name": "Favorite Bouqet 3",
-            "description": "This description is for bouqet 3",
-            "picture": "https://i.imgur.com/hMmLLC2.jpg",
-            "price": "3333"
-        },
-        {
-            "name": "Favorite Bouqet 4",
-            "description": "This description is for bouqet 4",
-            "picture": "https://i.imgur.com/TAPsYEH.jpg",
-            "price": "4444"
-        }
-    ]
-}
+        "data": [
+            {
+                "id": "1",
+                "name": "Favorite Bouqet 1",
+                "description": "This description is for bouqet 1",
+                "picture": "https://i.imgur.com/ScOiPDx.jpg",
+                "price": "1111"
+            },
+            {
+                "id": "2",
+                "name": "Favorite Bouqet 2",
+                "description": "This description is for bouqet 2",
+                "picture": "https://i.imgur.com/RgtoxyG.jpg",
+                "price": "2222"
+            },
+            {
+                "id": "3",
+                "name": "Favorite Bouqet 3",
+                "description": "This description is for bouqet 3",
+                "picture": "https://i.imgur.com/hMmLLC2.jpg",
+                "price": "3333"
+            },
+            {
+                "id": "4",
+                "name": "Favorite Bouqet 4",
+                "description": "This description is for bouqet 4",
+                "picture": "https://i.imgur.com/TAPsYEH.jpg",
+                "price": "4444"
+            }
+        ]
+    }
 
 
 @app.get('/season')
 def season_info():
     return {
-    "data": [
-        {
-            "name": "Season Bouqet 1",
-            "description": "This description is for bouqet 1",
-            "picture": "https://i.imgur.com/HnNqj2k.jpg",
-            "price": "1111"
-        },
-        {
-            "name": "Season Bouqet 2",
-            "description": "This description is for bouqet 2",
-            "picture": "https://i.imgur.com/EwKcxnl.jpg",
-            "price": "2222"
-        },
-        {
-            "name": "Season Bouqet 3",
-            "description": "This description is for bouqet 3",
-            "picture": "https://i.imgur.com/5PMLpcl.jpg",
-            "price": "3333"
-        },
-        {
-            "name": "Season Bouqet 4",
-            "description": "This description is for bouqet 4",
-            "picture": "https://i.imgur.com/vbSwDFa.jpg",
-            "price": "4444"
-        }
-    ]
-}
+        "data": [
+            {
+                "id": "5",
+                "name": "Season Bouqet 1",
+                "description": "This description is for bouqet 1",
+                "picture": "https://i.imgur.com/HnNqj2k.jpg",
+                "price": "1111"
+            },
+            {
+                "id": "6",
+                "name": "Season Bouqet 2",
+                "description": "This description is for bouqet 2",
+                "picture": "https://i.imgur.com/EwKcxnl.jpg",
+                "price": "2222"
+            },
+            {
+                "id": "7",
+                "name": "Season Bouqet 3",
+                "description": "This description is for bouqet 3",
+                "picture": "https://i.imgur.com/5PMLpcl.jpg",
+                "price": "3333"
+            },
+            {
+                "id": "8",
+                "name": "Season Bouqet 4",
+                "description": "This description is for bouqet 4",
+                "picture": "https://i.imgur.com/vbSwDFa.jpg",
+                "price": "4444"
+            }
+        ]
+    }
+
+@app.get('/flowers')
+def get_flowers():
+    return {
+        "data": [
+            {
+                "id": "1",
+                "name": "Favorite Bouqet 1",
+                "description": "This description is for bouqet 1",
+                "picture": "https://i.imgur.com/ScOiPDx.jpg",
+                "price": "1111"
+            },
+            {
+                "id": "2",
+                "name": "Favorite Bouqet 2",
+                "description": "This description is for bouqet 2",
+                "picture": "https://i.imgur.com/RgtoxyG.jpg",
+                "price": "2222"
+            },
+            {
+                "id": "3",
+                "name": "Favorite Bouqet 3",
+                "description": "This description is for bouqet 3",
+                "picture": "https://i.imgur.com/hMmLLC2.jpg",
+                "price": "3333"
+            },
+            {
+                "id": "4",
+                "name": "Favorite Bouqet 4",
+                "description": "This description is for bouqet 4",
+                "picture": "https://i.imgur.com/TAPsYEH.jpg",
+                "price": "4444"
+            },
+            {
+                "id": "5",
+                "name": "Season Bouqet 1",
+                "description": "This description is for bouqet 1",
+                "picture": "https://i.imgur.com/HnNqj2k.jpg",
+                "price": "1111"
+            },
+            {
+                "id": "6",
+                "name": "Season Bouqet 2",
+                "description": "This description is for bouqet 2",
+                "picture": "https://i.imgur.com/EwKcxnl.jpg",
+                "price": "2222"
+            },
+            {
+                "id": "7",
+                "name": "Season Bouqet 3",
+                "description": "This description is for bouqet 3",
+                "picture": "https://i.imgur.com/5PMLpcl.jpg",
+                "price": "3333"
+            },
+            {
+                "id": "8",
+                "name": "Season Bouqet 4",
+                "description": "This description is for bouqet 4",
+                "picture": "https://i.imgur.com/vbSwDFa.jpg",
+                "price": "4444"
+            }
+        ]
+    }
